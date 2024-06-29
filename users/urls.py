9bf7296ffebf4362c556d89ce1cd249676162a5e
marinam0406 <marinam0406@yandex.ru>
2024-06-29 17:27:49 +0300

    add 25.1

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet, PaymentListAPIView
@@ -8,5 +10,7 @@ app_name = UsersConfig.name
 router = DefaultRouter()
 router.register(r"user", UserViewSet, basename="user")
 urlpatterns = [
-    path("payments/", PaymentListAPIView.as_view(), name="payments")
+    path("payments/", PaymentListAPIView.as_view(), name="payments"),
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ] + router.urls
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User, Payment
@@ -11,6 +12,16 @@ from users.serializers import UserSerializer, PaymentSerializer
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
 
 
 class PaymentListAPIView(ListAPIView):
